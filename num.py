8b04b87e50da9ad2619260adc41e5ded5bd641c3
Vladislav Erzikov <erzikvv@gmail.com>
2024-04-12 13:19:37 +0300

    Update num.py

--- num.py
+++ num.py
@@ -6,3 +6,7 @@ def unique_elements(numbers):
 numbers_list = [1, 2, 2, 3, 4, 4, 5]
 unique_list = unique_elements(numbers_list)
 print(f"Уникальные элементы: {unique_list}")
+
+#Эта функция использует встроенный тип данных set в Python,
+#который автоматически удаляет все дубликаты,
+#и затем преобразует set обратно в list, чтобы сохранить тип данных списка. 
